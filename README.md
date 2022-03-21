81ae9d4d8b51de87e5d2f39e23f4510a5b0b8c43
Nguyen Van Tuan <tuan.ngv@kyanon.digital>
2022-03-21 07:36:43 +0700

    setup base project and ui home page

---
+++ README.md
@@ -0,0 +1,30 @@
+# NVT Portfolio
+
+# About
+
+a portfolio project of Nguyen Van Tuan, show case project work and information.
+
+# Install file with download.
+
+# run project
+
+- step 1: grunt uglify.
+- step 2: grunt sass.
+- step 3: grunt cssmin.
+- step 4: grunt.
+
+# Build using
+
+- Grunt
+- ThreeJS
+- LocomotiveJS
+- GSAP
+- Flickity
+
+# Tool
+
+- Prettier
+
+# Author
+
+- Nguyen Van Tuan - Frontend Developer.
---
+++ js/main.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+  // setup slider for feature projects
+  const projectSliders = () => {
+    const slideWrapper = $('main .feature-project .main-carousel');
+    let slider = slideWrapper.flickity({
+      // options
+      cellAlign: 'left',
+      wrapAround: true,
+      contain: true,
+      autoPlay: 2000,
+      pageDots: false,
+      prevNextButtons: false,
+    });
+  };
+  projectSliders();
+
+  // setup custom slider
+  const updateCursor = (e) => {
+    var cursor = $('.cursor');
+    let x = e.clientX;
+    let y = e.clientY;
+
+    cursor.css('top', `${y}px`);
+    cursor.css('left', `${x}px`);
+  };
+  $(window).on('mousemove', updateCursor);
+
+  // active cursor when hover
+  let activeCursor = () => {
+    const cursor = $('.cursor');
+    let styleActive = $('.style-active');
+    let styleSlide = $('.style-slide');
+    let styleView = $('.style-view');
+
+    styleActive.on('mouseleave', () => {
+      cursor.removeClass('cursor-active');
+    });
+    styleActive.on('mouseenter', () => {
+      cursor.addClass('cursor-active');
+    });
+
+    styleSlide.on('mouseleave', () => {
+      cursor.removeClass('cursor-slide');
+    });
+    styleSlide.on('mouseenter', () => {
+      cursor.addClass('cursor-slide');
+    });
+    styleView.on('mouseleave', () => {
+      cursor.removeClass('cursor-view');
+    });
+    styleView.on('mouseenter', () => {
+      cursor.addClass('cursor-view');
+    });
+  };
+  activeCursor();
+
+  // setup hover effect title main
+  let hoverEffectTranslateMatrix = () => {
+    const signature = $('footer .signature');
+    signature.tilt({
+      glare: true,
+      maxGlare: 0.5,
+    });
+  };
+  hoverEffectTranslateMatrix();
+});
+
+$(window).on('load', () => {
+  let c = 0;
+  setInterval(() => {
+    const count = $('.loading .count');
+    count.html(`${c++}%...`);
+  }, 40);
+
+  setTimeout(() => {
+    $('.loading').addClass('hide-loading');
+    $('.loading').children().css('visibility', 'hidden');
+  }, 2000);
+});
